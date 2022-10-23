81c3503120e6bca4f5251db6695f15398d42f40d
ruwiniw <ruwini.wijesekera@gmail.com>
2022-10-23 14:00:42 +0530

    Passport

--- models/user.js
+++ models/user.js
@@ -1,20 +1,24 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
 
 var User = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    }, 
-    password: {
-        type: String,
-        default: false
-    }, 
+    // username: {
+    //     type: String,
+    //     required: true,
+    //     unique: true
+    // }, 
+    // password: {
+    //     type: String,
+    //     default: false
+    // }, 
     admin: {
         type: Boolean,
         default: false
     }
 });
+
+//used username and hashed password
+User.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', User);
